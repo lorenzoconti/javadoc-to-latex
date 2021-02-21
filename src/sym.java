cc0378c60ee55852a4af1ef8da397b42ba910e41
lorenzoconti <dev.lorenzoconti@gmail.com>
2021-02-21 18:24:27 +0100

    add different keys

---
+++ src/sym.java
@@ -0,0 +1,25 @@
+public class sym {
+    // javadoc start
+    public static final int JDS = 6;
+    // javadoc end
+    public static final int JDE = 5;
+
+    // line of code
+    public static final int CODE = 4;
+
+    // line of text
+    public static final int TEXT = 11;
+
+    // end of file
+    public static final int EOF = -1;
+
+    // error
+    public static final int ERROR = 0;
+
+    // keys
+    public static final int KEY_PARAM = 10;
+    public static final int KEY_AUTHOR = 7;
+    public static final int KEY_EXCEPTION = 9;
+    public static final int KEY_CODE = 8;
+
+}
